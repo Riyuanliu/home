cbe2bf41ee2846fd9255efe1235064b95a03c8bd
Riyuan Liu <100328034+Riyuanliu@users.noreply.github.com>
2024-01-11 23:57:58 +0000

    Update Weather.vue

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -63,7 +63,7 @@ const getWeatherData = async () => {
       };
       weatherData.weather = {
         weather: result.weather[0].main || "UnknownW",
-        description: result.weather.description || "Unknown WD",
+        description: result.weather[0].description || "Unknown WD",
         temperature: result.main.temp || "Unknown Temp",
         windSpeed: result.wind.speed || "Unknown WS",
         windDirection: result.wind.deg || "Unknwn WD",
